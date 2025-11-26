2de359f351e6daac0bad39ea12023a573318acab
Al Mobin Reham <amsarkerreham@gmail.com>
2025-11-27 00:47:30 +0600

    added dtore

--- app/(public)/create-store/page.jsx
+++ app/(public)/create-store/page.jsx
@@ -33,7 +33,42 @@ export default function CreateStore() {
   };
 
   const fetchSellerStatus = async () => {
-    // Logic to check if the store is already submitted
+    const token = await getToken();
+    try {
+      const { data } = await axios.get("/api/store/create", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (["approved", "rejected", "pending"].includes(data.status)) {
+        setStatus(data.status);
+        setAlreadySubmitted(true);
+        switch (data.status) {
+          case "approved":
+            setMessage(
+              "Your store has been approved, you can now add products to your store from dashboard"
+            );
+            setTimeout(() => router.push("/store"), 5000);
+            break;
+          case "rejected":
+            setMessage(
+              "Your store request has been rejected, contact the admin for more details"
+            );
+            break;
+          case "pending":
+            setMessage(
+              "Your store request pending, please wait for admin to approve your store"
+            );
+            break;
+
+          default:
+            break;
+        }
+      } else {
+        setAlreadySubmitted(false);
+      }
+    } catch (error) {
+      toast.error(error?.response?.data?.error || error.message);
+    }
 
     setLoading(false);
   };
@@ -59,21 +94,27 @@ export default function CreateStore() {
         headers: { Authorization: `Bearer ${token}` },
       });
       toast.success(data.message);
+
+      await fetchSellerStatus();
     } catch (error) {
       toast.error(error?.response?.data?.error || error.message);
     }
   };
 
   useEffect(() => {
-    fetchSellerStatus();
-  }, []);
+    if (user) {
+      fetchSellerStatus();
+    }
+  }, [user]);
 
-  if(!user) {
+  if (!user) {
     return (
-        <div className="min-h-[80vh] mx-6 flex items-center justify-center text-slate-400">
-            <h1 className="text-2xl sm:text-4xl font-semibold">Please <span className="text-slate-500">Login</span> To Continue</h1>
-        </div>
-    )
+      <div className="min-h-[80vh] mx-6 flex items-center justify-center text-slate-400">
+        <h1 className="text-2xl sm:text-4xl font-semibold">
+          Please <span className="text-slate-500">Login</span> To Continue
+        </h1>
+      </div>
+    );
   }
 
   return !loading ? (
@@ -206,3 +247,4 @@ export default function CreateStore() {
     <Loading />
   );
 }
+
