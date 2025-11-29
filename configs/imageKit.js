c315dd30af84a10fbd5bf36faad370658e6a698b
Al Mobin Reham <amsarkerreham@gmail.com>
2025-11-29 23:19:06 +0600

    edit e

--- configs/imageKit.js
+++ configs/imageKit.js
@@ -1,7 +1,7 @@
 import ImageKit from "imagekit";
 
 var imagekit = new ImageKit ({
-    publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
     urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
 })
