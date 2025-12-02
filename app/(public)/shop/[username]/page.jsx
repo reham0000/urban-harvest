4cda3748d25569ec70f7456d59c05b8777808e5e
Al Mobin Reham <amsarkerreham@gmail.com>
2025-12-02 20:59:46 +0600

    added rating feature

--- app/(public)/shop/[username]/page.jsx
+++ app/(public)/shop/[username]/page.jsx
@@ -5,7 +5,8 @@ import { useEffect, useState } from "react"
 import { MailIcon, MapPinIcon } from "lucide-react"
 import Loading from "@/components/Loading"
 import Image from "next/image"
-import { dummyStoreData, productDummyData } from "@/assets/assets"
+import axios from "axios"
+import toast from "react-hot-toast"
 
 export default function StoreShop() {
 
@@ -15,8 +16,13 @@ export default function StoreShop() {
     const [loading, setLoading] = useState(true)
 
     const fetchStoreData = async () => {
-        setStoreInfo(dummyStoreData)
-        setProducts(productDummyData)
+        try {
+            const {data} = await axios.get(`/api/store/data?username=${username}`)
+            setStoreInfo(data.store)
+            setProducts(data.store.product)
+        } catch (error) {
+            toast.error(error?.response?.data?.error || error.message)
+        }
         setLoading(false)
     }
 
@@ -36,6 +42,7 @@ export default function StoreShop() {
                         className="size-32 sm:size-38 object-cover border-2 border-slate-100 rounded-md"
                         width={200}
                         height={200}
+                        priority 
                     />
                     <div className="text-center md:text-left">
                         <h1 className="text-3xl font-semibold text-slate-800">{storeInfo.name}</h1>
@@ -60,9 +67,89 @@ export default function StoreShop() {
             <div className=" max-w-7xl mx-auto mb-40">
                 <h1 className="text-2xl mt-12">Shop <span className="text-slate-800 font-medium">Products</span></h1>
                 <div className="mt-5 grid grid-cols-2 sm:flex flex-wrap gap-6 xl:gap-12 mx-auto">
-                    {products.map((product) => <ProductCard key={product.id} product={product} />)}
+                    {/* {products.map((product) => <ProductCard key={product.id} product={product} />)} */}
+                    {(products || []).map((product) => <ProductCard key={product.id} product={product} />)}
                 </div>
             </div>
         </div>
     ) : <Loading />
 }
+
+// 'use client'
+// import ProductCard from "@/components/ProductCard"
+// import { useParams } from "next/navigation"
+// import { useEffect, useState } from "react"
+// import { MailIcon, MapPinIcon } from "lucide-react"
+// import Loading from "@/components/Loading"
+// import Image from "next/image"
+// import axios from "axios"
+// import toast from "react-hot-toast"
+
+// export default function StoreShop() {
+
+//     const { username } = useParams()
+//     const [products, setProducts] = useState([])
+//     const [storeInfo, setStoreInfo] = useState(null)
+//     const [loading, setLoading] = useState(true)
+
+//     const fetchStoreData = async () => {
+//         try {
+//             const {data} = await axios.get(`/api/store/data?username=${username}`)
+//             if(data.store){
+//                 setStoreInfo(data.store)
+//                 setProducts(data.store.product || [])
+//             } else {
+//                 toast.error("Store not found")
+//             }
+//         } catch (error) {
+//             toast.error(error?.response?.data?.error || error.message)
+//         }
+//         setLoading(false)
+//     }
+
+//     useEffect(() => {
+//         if(username) fetchStoreData()
+//     }, [username])
+
+//     if(loading) return <Loading />
+
+//     return (
+//         <div className="min-h-[70vh] mx-6">
+
+//             {/* Store Info Banner */}
+//             {storeInfo && (
+//                 <div className="max-w-7xl mx-auto bg-slate-50 rounded-xl p-6 md:p-10 mt-6 flex flex-col md:flex-row items-center gap-6 shadow-xs">
+//                     <Image
+//                         src={storeInfo.logo}
+//                         alt={storeInfo.name}
+//                         className="w-32 h-32 sm:w-38 sm:h-38 object-cover border-2 border-slate-100 rounded-md"
+//                         width={200}
+//                         height={200}
+//                     />
+//                     <div className="text-center md:text-left">
+//                         <h1 className="text-3xl font-semibold text-slate-800">{storeInfo.name}</h1>
+//                         <p className="text-sm text-slate-600 mt-2 max-w-lg">{storeInfo.description}</p>
+//                         <div className="space-y-2 text-sm text-slate-500 mt-4">
+//                             <div className="flex items-center">
+//                                 <MapPinIcon className="w-4 h-4 text-gray-500 mr-2" />
+//                                 <span>{storeInfo.address}</span>
+//                             </div>
+//                             <div className="flex items-center">
+//                                 <MailIcon className="w-4 h-4 text-gray-500 mr-2" />
+//                                 <span>{storeInfo.email}</span>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             )}
+
+//             {/* Products */}
+//             <div className=" max-w-7xl mx-auto mb-40">
+//                 <h1 className="text-2xl mt-12">Shop <span className="text-slate-800 font-medium">Products</span></h1>
+//                 <div className="mt-5 grid grid-cols-2 sm:flex flex-wrap gap-6 xl:gap-12 mx-auto">
+//                     {(products || []).map((product) => <ProductCard key={product.id} product={product} />)}
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
---
+++ app/api/orders/route.js
@@ -0,0 +1,166 @@
+import prisma from "@/lib/prisma";
+import { getAuth } from "@clerk/nextjs/server";
+import { PaymentMethod } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  try {
+    const { userId, has } = getAuth(request);
+    if (!userId) {
+      return NextResponse.json({ error: "not authorized" }, { status: 401 });
+    }
+
+    const { addressId, items, couponCode, paymentMethod } =
+      await request.json();
+
+    // check if all required fields are present
+    if (
+      !addressId ||
+      !paymentMethod ||
+      !items ||
+      !Array.isArray(items) ||
+      items.length === 0
+    ) {
+      return NextResponse.json(
+        { error: "missing order details" },
+        { status: 401 }
+      );
+    }
+
+    let coupon = null;
+    if (couponCode) {
+      coupon = await prisma.coupon.findUnique({                                                                         
+        where: { code: couponCode }
+      });
+      if (!coupon) {
+        return NextResponse.json(
+          { error: "Coupon not found" },
+          { status: 400 }
+        );
+      }
+    }
+
+    // check if coupon is applicable for new users
+    if (couponCode && coupon.forNewUser) {
+      const userorders = await prisma.order.findMany({
+        where: { userId },
+      });
+      if (userorders.length > 0) {
+        return NextResponse.json(
+          { error: "Coupon valid for new users" },
+          { status: 400 }
+        );
+      }
+    }
+
+    const isPlusMember = has({ plan: "plus" });
+
+    // check if coupon is applicable for members
+    if (couponCode && coupon.forMember) {
+      if (!isPlusMember) {
+        return NextResponse.json(
+          { error: "Coupon valid for members only" },
+          { status: 400 }
+        );
+      }
+    }
+
+    // group orders by storedId using a Map
+    const ordersByStore = new Map();
+
+    for (const item of items) {
+      const product = await prisma.product.findUnique({
+        where: { id: item.id },
+      });
+      const storeId = product.storeId;
+      if (!ordersByStore.has(storeId)) {
+        ordersByStore.set(storeId, []);
+      }
+      ordersByStore.get(storeId).push({ ...item, price: product.price });
+    }
+
+    let orderIds = [];
+    let fullAmount = 0;
+
+    let isShippingFeeAdded = false;
+
+    // Create orders for each seller
+    for (const [storeId, sellerItems] of ordersByStore.entries()) {
+      let total = sellerItems.reduce(
+        (acc, item) => acc + (item.price * item.quantity) ,
+        0
+      );
+
+      if (couponCode) {
+        total -= (total * coupon.discount) / 100;
+      }
+      if (!isPlusMember && !isShippingFeeAdded) {
+        total += 5;
+        isShippingFeeAdded = true;
+      }
+
+      fullAmount += parseFloat(total.toFixed(2));
+
+      const order = await prisma.order.create({
+        data: {
+          userId,
+          storeId,
+          addressId,
+          total: parseFloat(total.toFixed(2)),
+          paymentMethod,
+          isCouponUsed: coupon ? true : false,
+          coupon: coupon ? coupon : {},
+          orderItems: {
+            create: sellerItems.map((item) => ({
+              productId: item.id,
+              quantity: item.quantity,
+              price: item.price,
+            })),
+          },
+        },
+      });
+      orderIds.push(order.id);
+    }
+
+    // clear the cart
+    await prisma.user.update({
+      where: { id: userId },
+      data: { cart: {} },
+    });
+
+    return NextResponse.json({ message: "Order placed successfully" });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: error.code || error.message },
+      { status: 400 }
+    );
+  }
+}
+
+// Get all orders for a user
+export async function GET(request) {
+  try {
+    const {userId} = getAuth(request)
+    const orders = await prisma.order.findMany({
+        where: {userId, OR: [
+            {paymentMethod: PaymentMethod.COD},
+            {AND: [{paymentMethod: PaymentMethod.STRIPE}, {isPaid: true}]}
+        ]},
+        include: {
+            orderItems: {include: {product: true}},
+            address: true
+        },
+        orderBy: {createdAt: 'desc'}
+    })
+
+    return NextResponse.json({orders})
+
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: error.message },
+      { status: 400 }
+    );
+  }
+}
